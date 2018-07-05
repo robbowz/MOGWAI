8adbbb5a55f439b9b5bc938faeadd41e88d1005d
Robert <36878317+robbowz@users.noreply.github.com>
2018-07-05 10:28:33 +0200

    Update mogwai_ubuntu_install.sh

--- mogwai_ubuntu_install.sh
+++ mogwai_ubuntu_install.sh
@@ -1,18 +1,18 @@
-# teshub masternode install script
+# mogwai masternode install script
 # Edited by Robbowz
-VERSION="0.2"
-NODEPORT='23500'
-RPCPORT='23502'
+VERSION="0.1"
+NODEPORT='17777'
+RPCPORT='17710'
 
 # Useful variables
 declare -r DATE_STAMP="$(date +%y-%m-%d-%s)"
-declare -r SCRIPT_LOGFILE="/tmp/teshub_node_${DATE_STAMP}_out.log"
+declare -r SCRIPT_LOGFILE="/tmp/mogwai_node_${DATE_STAMP}_out.log"
 declare -r SCRIPTPATH=$( cd $(dirname ${BASH_SOURCE[0]}) > /dev/null; pwd -P )
 declare -r WANIP=$(dig +short myip.opendns.com @resolver1.opendns.com)
 
 
 function print_greeting() {
-	echo -e "[0;35m teshub masternode install script[0m\n"
+	echo -e "[0;35m mogwai masternode install script[0m\n"
 }
 
 
@@ -41,11 +41,11 @@ function install_packages() {
 function swaphack() {
 	echo "Setting up disk swap..."
 	free -h
-	rm -f /var/teshub_node_swap.img
-	touch /var/teshub_node_swap.img
-	dd if=/dev/zero of=/var/teshub_node_swap.img bs=1024k count=2000 &>> ${SCRIPT_LOGFILE}
-	chmod 0600 /var/teshub_node_swap.img
-	mkswap /var/teshub_node_swap.img &>> ${SCRIPT_LOGFILE}
+	rm -f /var/mogwai_node_swap.img
+	touch /var/mogwai_node_swap.img
+	dd if=/dev/zero of=/var/mogwai_node_swap.img bs=1024k count=2000 &>> ${SCRIPT_LOGFILE}
+	chmod 0600 /var/mogwai_node_swap.img
+	mkswap /var/mogwai_node_swap.img &>> ${SCRIPT_LOGFILE}
 	free -h
 	echo "Swap setup complete..."
 }
@@ -53,26 +53,35 @@ function swaphack() {
 
 function remove_old_files() {
 	echo "Removing old files..."
-	sudo killall teshubd
-	sudo rm -rf /root/teshub
-	sudo rm -rf /root/.teshub
-	sudo rm -rf /root/.teshubcore
-    	sudo rm -rf teshubd
-    	sudo rm -rf teshub-cli
+	sudo killall mogwaid
+	sudo rm -rf /root/mogwai
+	sudo rm -rf /root/.mogwaicore
+	sudo rm -rf /root/.mogwaicorecore
+   	sudo rm -rf mogwaid
+   	sudo rm -rf mogwai-cli
 	echo "Done..."
 }
 
 
+function install_sentinel() {
+  echo -e "Install sentinel."
+  apt-get -y install python-virtualenv virtualenv >/dev/null 2>&1
+  git clone https://github.com/mogwaicoin/mogwai-sentinel.git .mogwaicorecore/sentinel >/dev/null 2>&1
+  cd .mogwaicorecore/sentinel
+  virtualenv ./venv >/dev/null 2>&1
+  ./venv/bin/pip install -r requirements.txt >/dev/null 2>&1
+  echo  "* * * * * cd .mogwaicorecore/sentinel && ./venv/bin/python bin/sentinel.py >> .mogwaicorecore/sentinel.log 2>&1" > /root/.mogwaicorecore/mogwai.cron
+  crontab /root/.mogwaicorecore/mogwai.cron
+  rm /root/.mogwaicorecore/mogwai.cron >/dev/null 2>&1
+}
+
 function download_wallet() {
 	echo "Downloading wallet..."
-	mkdir /root/teshub
-    	cd teshub
-	mkdir /root/.teshub
-	wget https://github.com/Teshubcoin/wallet/releases/download/v1.0.1/tesb-wallet-linux.tar.gz
-	tar -zxvf tesb-wallet-linux.tar.gz
-	cp p0sfix/teshubd /root/teshub/teshubd
-	cp p0sfix/teshub-cli /root/teshub/teshub-cli
-	rm -rf p0sfix/
+	mkdir /root/mogwai
+    	cd mogwai
+	mkdir /root/.mogwaicorecore
+	wget https://github.com/mogwaicoin/mogwai/releases/download/untagged-f2812049204fdc70402c/mogwaicore-0.12.2-linux64.tar.gz
+    	tar -xvf mogwaicore-0.12.2-linux64.tar.gz
 	echo "Done..."
 }
 
@@ -84,8 +93,8 @@ function configure_firewall() {
 	ufw default deny			&>> ${SCRIPT_LOGFILE}
 	ufw logging on				&>> ${SCRIPT_LOGFILE}
 	ufw allow ssh/tcp			&>> ${SCRIPT_LOGFILE}
-	ufw allow 23500/tcp			&>> ${SCRIPT_LOGFILE}
-	ufw allow 23502/tcp			&>> ${SCRIPT_LOGFILE}
+	ufw allow 17777/tcp			&>> ${SCRIPT_LOGFILE}
+	ufw allow 17710tcp			&>> ${SCRIPT_LOGFILE}
 	# This will only allow 6 connections every 30 seconds from the same IP address.
 	ufw limit OpenSSH			&>> ${SCRIPT_LOGFILE}
 	ufw --force enable			&>> ${SCRIPT_LOGFILE}
@@ -95,44 +104,43 @@ function configure_firewall() {
 
 function configure_masternode() {
 	echo "Configuring masternode..."
-	conffile=/root/.teshub/teshub.conf
+	conffile=/root/.mogwaicore/mogwai.conf
 	PASSWORD=`pwgen -1 20 -n` &>> ${SCRIPT_LOGFILE}
 	if [ "x$PASSWORD" = "x" ]; then
 	    PASSWORD=${WANIP}-`date +%s`
 	fi
 	echo "Loading and syncing wallet..."
 	echo "    if you see *error: Could not locate RPC credentials* message, do not worry"
-	/root/teshub/teshub-cli stop
+	/root/mogwai/mogwai-cli stop
 	echo "It's okay."
 	sleep 10
-	echo -e "rpcuser=teshubuser\nrpcpassword=${PASSWORD}\nrpcport=${RPCPORT}\nport=${NODEPORT}\nexternalip=${WANIP}\nlisten=1\nmaxconnections=250" >> ${conffile}
+	echo -e "rpcuser=mogwaiuser\nrpcpassword=${PASSWORD}\nrpcport=${RPCPORT}\nport=${NODEPORT}\nexternalip=${WANIP}\nlisten=1\nmaxconnections=250" >> ${conffile}
 	echo ""
 	echo -e "[0;35m==================================================================[0m"
 	echo -e "     DO NOT CLOSE THIS WINDOW OR TRY TO FINISH THIS PROCESS"
 	echo -e "                        PLEASE WAIT 2 MINUTES"
 	echo -e "[0;35m==================================================================[0m"
 	echo ""
-	/root/teshub/teshubd -daemon
+	/root/mogwai/mogwaid -daemon
 	echo "2 MINUTES LEFT"
 	sleep 60
 	echo "1 MINUTE LEFT"
 	sleep 60
-	masternodekey=$(/root/teshub/teshub-cli masternode genkey)
-	/root/teshub/teshub-cli stop
+	masternodekey=$(/root/mogwai/mogwai-cli masternode genkey)
+	/root/mogwai/mogwai-cli stop
 	sleep 20
 	echo "Creating masternode config..."
 	echo -e "daemon=1\nmasternode=1\nmasternodeprivkey=$masternodekey" >> ${conffile}
 	echo "Done...Starting daemon..."
-	/root/teshub/teshubd -daemon
+	/root/mogwai/mogwaid -daemon
 }
-
 
 function show_result() {
 	echo ""
 	echo -e "[0;35m==================================================================[0m"
 	echo "DATE: ${DATE_STAMP}"
 	echo "LOG: ${SCRIPT_LOGFILE}"
-	echo "rpcuser=teshubuser"
+	echo "rpcuser=mogwaiuser"
 	echo "rpcpassword=${PASSWORD}"
 	echo ""
 	echo -e "[0;35m INSTALLED WITH VPS IP: ${WANIP}:${NODEPORT} [0m"
@@ -151,8 +159,8 @@ function cleanup() {
 }
 
 
-#Setting auto start cron job for teshubd
-cronjob="@reboot sleep 30 && /root/teshub/teshubd"
+#Setting auto start cron job for mogwaid
+cronjob="@reboot sleep 30 && /root/mogwai/mogwaid"
 crontab -l > tempcron
 if ! grep -q "$cronjob" tempcron; then
     echo -e "Configuring crontab job..."
@@ -211,4 +219,4 @@ show_result
 cleanup
 echo "All done!"
 cd ~/
-sudo rm /root/teshub_ubuntu_install.sh
+sudo rm /root/mogwai_ubuntu_install.sh
